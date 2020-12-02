64087df855748ba62ecf6ca738ec0eb5137c2cbb
Nicolas <ntorres@codigodelsur.com>
2020-12-02 17:23:53 -0300

    add more logic to capture burron

--- DronelinkCoreUI/CaptureButton.swift
+++ DronelinkCoreUI/CaptureButton.swift
@@ -31,12 +31,18 @@ public class CaptureButton: UIButton {
     }
     
     func configeBtn() {
+        setImage(DronelinkUI.loadImage(named: "captureIcon")?.withRenderingMode(.alwaysOriginal), for: .normal)
         self.addTarget(self, action: #selector(btnClicked(_:)), for: .touchUpInside)
     }
     
+    private func cameraIsCapturing() -> Bool {
+        // Check if the camera is capturing
+        return droneSessionManager?.session?.cameraState(channel: 0)?.value.isCapturing == true
+    }
+    
     @objc func btnClicked (_ sender:UIButton) {
-        var command = Kernel.StartCaptureCameraCommand()
-        try? self.droneSessionManager.session?.add(command: command)
+        // If camera is capturing then add stop capture command else add start capture command
+        try? self.droneSessionManager.session?.add(command: cameraIsCapturing() ? Kernel.StopCaptureCameraCommand() : Kernel.StartCaptureCameraCommand())
     }
     
 }
