3f3bba940c14e984ed3e7ffd9b3c80d34fb755d2
Nicolas <ntorres@codigodelsur.com>
2020-12-03 13:01:27 -0300

    add unit system command

---
+++ DronelinkCoreUI/UnitSystemControl.swift
@@ -0,0 +1,47 @@
+//
+//  UnitSystemControl.swift
+//  DronelinkCoreUI
+//
+//  Created by Nicolas Torres on 3/12/20.
+//  Copyright © 2020 Dronelink. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import DronelinkCore
+
+public class UnitSystemControl: UISegmentedControl {
+    
+    private var droneSessionManager: DroneSessionManager!
+    
+    public static func create(droneSessionManager: DroneSessionManager) -> UnitSystemControl {
+        let unitSystemControl = UnitSystemControl(items: ["metric", "imperial"])
+        unitSystemControl.droneSessionManager = droneSessionManager
+        return unitSystemControl
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configeControl()
+    }
+    
+    required init(items: [String]) {
+        super.init(items: items)
+        configeControl()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configeControl()
+    }
+    
+    func configeControl() {
+        self.addTarget(self, action: #selector(segmentedControlValueChanged(segment:)), for: .valueChanged)
+    }
+    
+    @objc func segmentedControlValueChanged(segment: UISegmentedControl) {
+        var command = Kernel.UnitSystemCommand()
+        command.unit = segment.selectedSegmentIndex == 0 ? .metric : .imperial
+        try? self.droneSessionManager.session?.add(command: command)
+    }
+}
