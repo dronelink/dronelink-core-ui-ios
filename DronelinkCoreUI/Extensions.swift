2f640b74c6a2ed602c9abe1f761db0c926053be4
Bryan Taylor <bryan@dronelink.com>
2023-06-14 09:37:25 -0500

    Added String localization override code

--- DronelinkCoreUI/Extensions.swift
+++ DronelinkCoreUI/Extensions.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Dronelink. All rights reserved.
 //
 import Foundation
+import DronelinkCore
 import JavaScriptCore
 import CoreLocation
 import UIKit
@@ -17,10 +18,42 @@ extension String {
     internal static let LocalizationMissing = "MISSING STRING LOCALIZATION"
     
     var localized: String {
+        if let language = Dronelink.shared.language {
+            // if system language is selected
+            if language == "" {
+                let value = DronelinkUI.bundle.localizedString(forKey: self, value: String.LocalizationMissing, table: nil)
+                //assert(value != String.LocalizationMissing, "String localization missing: \(self)")
+                return value
+            }
+
+            // override language if possible
+            let languagePath = Bundle.main.path(forResource: language, ofType: "lproj")
+            if (languagePath != nil) {
+                let bundle = Bundle(path: languagePath!)
+                return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: String.LocalizationMissing, comment: "")
+            }
+            
+            
+            // use english if override language isn't available
+            let english = "en"
+            let englishPath = Bundle.main.path(forResource: english, ofType: "lproj")
+            if (englishPath != nil) {
+                let bundle = Bundle(path: englishPath!)
+                return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: String.LocalizationMissing, comment: "")
+            }
+        }
+
+        // fall back on system language if lang does not exist (perhaps because the user isn't logged in yet)
         let value = DronelinkUI.bundle.localizedString(forKey: self, value: String.LocalizationMissing, table: nil)
         //assert(value != String.LocalizationMissing, "String localization missing: \(self)")
         return value
     }
+
+//    var localized: String {
+//        let value = DronelinkUI.bundle.localizedString(forKey: self, value: String.LocalizationMissing, table: nil)
+//        //assert(value != String.LocalizationMissing, "String localization missing: \(self)")
+//        return value
+//    }
 }
 
 extension UIApplication {
